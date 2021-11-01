5000c3ca9080cfdb96f072a5fbb6dfbc782d51c9
Adam Novak <anovak@soe.ucsc.edu>
2021-11-01 09:18:26 -0700

    Fix timestamp dump

--- rebuild.sh
+++ rebuild.sh
@@ -7,7 +7,7 @@ git branch -D quay-rebuild || true
 git checkout -b quay-rebuild
 date > dind-build-timestamp.txt
 git add dind-build-timestamp.txt
-git commit -m "Update build timestamp to $(cat prebake-build-timestamp.txt)"
+git commit -m "Update build timestamp to $(cat dind-build-timestamp.txt)"
 git push git@github.com:vgteam/dind.git quay-rebuild:master
 
 
